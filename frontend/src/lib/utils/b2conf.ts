757b07f5124f75af2e89fe3d624544c1f9a42743
John Doe <sfoqus@gmail.com>
2024-09-30 13:46:50 +0400

    change b2c0nf

--- frontend/src/lib/utils/b2conf.ts
+++ frontend/src/lib/utils/b2conf.ts
@@ -1,7 +1,7 @@
 import B2 from 'backblaze-b2';
 const b2 = new B2({
-	applicationKeyId: process.env.B2_APPLICATION_KEY_ID!,
-	applicationKey: process.env.B2_APPLICATION_KEY!,
+	applicationKeyId: process.env.NEXT_PUBLIC_B2_APPLICATION_KEY_ID!,
+	applicationKey: process.env.NEXT_PUBLIC_B2_APPLICATION_KEY!,
 })
 
 
--- frontend/src/lib/utils/uploadFile.ts
+++ frontend/src/lib/utils/uploadFile.ts
@@ -3,13 +3,10 @@ import b2 from './b2conf';
 
 export const uploadFile = async (file: File) => {
   try {
-		console.log(process.env.B2_BUCKET_NAME, 'thj')
-    await b2.authorize(
-
-		);
+    await b2.authorize();
 
     const { data: { uploadUrl, authorizationToken } } = await b2.getUploadUrl({
-      bucketId: process.env.B2_BUCKET_ID!,
+      bucketId: process.env.NEXT_PUBLIC_B2_BUCKET_ID!,
     });
 
     const fileName = `${Date.now()}-${file.name}`;
@@ -23,7 +20,7 @@ export const uploadFile = async (file: File) => {
       contentType: file.type,
     });
 
-    const fileUrl = `https://f002.backblazeb2.com/file/${process.env.B2_BUCKET_NAME}/${fileName}`;
+    const fileUrl = `https://f002.backblazeb2.com/file/${process.env.NEXT_PUBLIC_B2_BUCKET_NAME}/${fileName}`;
     return fileUrl;
   } catch (error) {
     console.error('Error uploading file to B2:', error);
